e1193d5c88e61eb28e4bab97edf114770d658cfc
MoOmEeN <moomeen@gmail.com>
2017-01-24 01:13:27 +0100

    ignore unknown

--- src/main/java/com/keendly/model/DeliveryRequest.java
+++ src/main/java/com/keendly/model/DeliveryRequest.java
@@ -1,7 +1,10 @@
 package com.keendly.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DeliveryRequest {
 
     public Long id;
