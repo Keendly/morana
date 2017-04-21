b58359e926456cf5b407fa3c854073817a79146c
MoOmEeN <moomeen@gmail.com>
2017-04-21 02:08:26 +0200

    cleanup

--- src/main/java/com/keendly/Preprocessor.java
+++ src/main/java/com/keendly/Preprocessor.java
@@ -15,36 +15,18 @@ public class Preprocessor {
 
     public void preprocess(){
         removeBrokenAttributes();
-        remvoeSVGs();
+        tryToAvoidSegFault();
     }
 
-    private void remvoeSVGs(){
-        Elements elements = document.getElementsByTag("svg");
-        elements.remove();
-//        for (Element element : elements){
-//            Elements children = element.getAllElements();
-//            for (Element child : children){
-//                if (child.tagName().equals("use")){
-//                    child.remove();
-//                }
-//
-//                for (Attribute attribute : child.attributes()){
-//                    if (attribute.getKey().equals("xmlns")){
-//                        element.removeAttr(attribute.getKey());
-//                    }
-//                }
-//            }
-//        }
+    private void tryToAvoidSegFault(){
+        document.getElementsByTag("svg").remove();
+        document.getElementsByTag("picture").remove();
     }
 
     private void removeBrokenAttributes(){
         Elements elements = document.getAllElements();
 
         for (Element element : elements){
-            if (element.tagName().equals("picture")){
-                element.remove();
-            }
-
             for (Attribute attribute : element.attributes()){
                 // remove attributes that have < or > in value because they cause kindlegen crash
                 if (attribute.getValue().contains(">") || attribute.getValue().contains("<")){
